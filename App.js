7accc47631a8732bb6b73e3c9b65c09f3f3c9f13
Gilang Ramadhan <gilang.ramadhan011297+github@gmail.com>
2022-09-30 00:13:10 +0700

    Adding Search Component

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import './App.css';
-import Navbar from './components/Navbar'
-import Home from './components/Home'
-import AboutView from './components/AboutView'
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import AboutView from './components/AboutView';
+import SearchView from './components/SearchView';
 import { Routes, Route } from 'react-router-dom';
 
 
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path="/home" element={<Home />} />
           <Route path="/about" element={<AboutView />} />
+          <Route path="/search" element={<SearchView />} />
         </Routes>
     </div>
   );
---
+++ SearchView.js
@@ -0,0 +1,13 @@
+import Hero from "./Hero";
+
+// adding functional home
+const SearchView = () => {
+  return (
+    <>
+      <Hero text="Search for.." />
+      
+    </>
+  );
+};
+
+export default SearchView;
