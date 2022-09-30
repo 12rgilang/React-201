d352914c934d114a93ee8de8acb7aacf29e23063
Gilang Ramadhan <gilang.ramadhan011297+github@gmail.com>
2022-09-30 15:28:27 +0700

    Component props

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import AboutView from './components/AboutView';
@@ -6,16 +7,25 @@ import SearchView from './components/SearchView';
 import { Routes, Route } from 'react-router-dom';
 
 
-
-// installing React router
 function App() {
+
+  const [searchResults, setSearchResults] = useState([]);
+  const [searchText, setSearchText] = useState ('');
+
+  // console.log(searchText, "is the default")
+  // setTimeout(() => {
+  //   setSearchText("New text")
+  //   console.log(searchText, "is the new text")
+  // }, 2000)
+  
+
   return (
     <div>
       <Navbar />
         <Routes>
           <Route path="/home" element={<Home />} />
           <Route path="/about" element={<AboutView />} />
-          <Route path="/search" element={<SearchView />} />
+          <Route path="/search" element={<SearchView keyword={searchText} searchResults={searchResults}/>} />
         </Routes>
     </div>
   );
--- SearchView.js
+++ SearchView.js
@@ -1,10 +1,12 @@
 import Hero from "./Hero";
 
 // adding functional home
-const SearchView = () => {
+const SearchView = ({ keyword, SearchResults}) => {
+  const title = `You are searching for ${keyword}`
+  console.log(SearchResults, "are the search results")
   return (
     <>
-      <Hero text="Search for.." />
+      <Hero text={title} />
       
     </>
   );
