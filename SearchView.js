decf21b9a3714f7e52b32dd6976e13391ce40ddf
Gilang Ramadhan <gilang.ramadhan011297+github@gmail.com>
2022-09-30 23:25:32 +0700

    Getting API Keys from TMDB

--- SearchView.js
+++ SearchView.js
@@ -1,5 +1,11 @@
 import Hero from "./Hero";
 
+// TMDB API KEY = 18e659b9c7680f677bc6ab62bf5a8838
+// Example link for movie searches = https://api.themoviedb.org/3/search/movie?api_key=18e659b9c7680f677bc6ab62bf5a8838&language=en-US&query=Harry%20Potter&page=1&include_adult=false
+
+
+
+
 // Binding Keyup events
 const SearchView = ({ keyword, SearchResults }) => {
   const title = `You are searching for ${keyword}`;
